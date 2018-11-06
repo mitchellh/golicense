534340999beb81cd6fc78fa4c0bea7870abef5f1
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-11-05 18:00:40 -0800

    wip

---
+++ license/finder.go
@@ -0,0 +1,35 @@
+package license
+
+import (
+	"github.com/hashicorp/go-multierror"
+	"github.com/mitchellh/golicense/module"
+)
+
+// Finder implementations can find a license for a given module.
+type Finder interface {
+	// License looks up the license for a given module.
+	License(module.Module) (*License, error)
+}
+
+// Find finds the license for the given module using a set of finders.
+//
+// The finders are tried in the order given. The first finder to return
+// a non-nil License without an error is returned. If a finder returns
+// an error, other finders are still attempted. It is possible for a non-nil
+// license to be returned WITH a non-nil error meaning a different lookup
+// failed.
+func Find(m module.Module, fs []Finder) (r *License, rerr error) {
+	for _, f := range fs {
+		lic, err := f.License(m)
+		if err != nil {
+			rerr = multierror.Append(rerr, err)
+			continue
+		}
+		if lic != nil {
+			r = lic
+			break
+		}
+	}
+
+	return
+}
---
+++ license/license.go
@@ -0,0 +1,7 @@
+package license
+
+// License represents a software license.
+type License struct {
+	Name string // Name is a human-friendly name like "MIT License"
+	SPDX string // SPDX ID of the license, blank if unknown or unavailable
+}
