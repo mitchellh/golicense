3853e3116589c165179fdc6dda668aa720eb8a57
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-11-17 16:15:32 -0800

    Add SPDX ID to XLSX output

--- output_xlsx.go
+++ output_xlsx.go
@@ -58,12 +58,14 @@ func (o *XLSXOutput) Close() error {
 	// Headers
 	f.SetCellValue(s, "A1", "Dependency")
 	f.SetCellValue(s, "B1", "Version")
-	f.SetCellValue(s, "C1", "License")
-	f.SetCellValue(s, "D1", "Allowed")
+	f.SetCellValue(s, "C1", "SPDX ID")
+	f.SetCellValue(s, "D1", "License")
+	f.SetCellValue(s, "E1", "Allowed")
 	f.SetColWidth(s, "A", "A", 40)
 	f.SetColWidth(s, "B", "B", 20)
-	f.SetColWidth(s, "C", "C", 40)
-	f.SetColWidth(s, "D", "D", 10)
+	f.SetColWidth(s, "C", "C", 20)
+	f.SetColWidth(s, "D", "D", 40)
+	f.SetColWidth(s, "E", "E", 10)
 
 	// Create all our styles
 	redStyle, _ := f.NewStyle(`{"fill":{"type":"pattern","pattern":1,"color":["#FFCCCC"]}}`)
@@ -86,11 +88,12 @@ func (o *XLSXOutput) Close() error {
 		m := index[k]
 		f.SetCellValue(s, "A"+row, m.Path)
 		f.SetCellValue(s, "B"+row, m.Version)
-		f.SetCellValue(s, "D"+row, "unknown")
+		f.SetCellValue(s, "E"+row, "unknown")
 		f.SetCellStyle(s, "A"+row, "A"+row, yellowStyle)
 		f.SetCellStyle(s, "B"+row, "B"+row, yellowStyle)
 		f.SetCellStyle(s, "C"+row, "C"+row, yellowStyle)
 		f.SetCellStyle(s, "D"+row, "D"+row, yellowStyle)
+		f.SetCellStyle(s, "E"+row, "E"+row, yellowStyle)
 
 		raw := o.modules[m]
 		if raw == nil {
@@ -99,38 +102,45 @@ func (o *XLSXOutput) Close() error {
 			f.SetCellStyle(s, "B"+row, "B"+row, redStyle)
 			f.SetCellStyle(s, "C"+row, "C"+row, redStyle)
 			f.SetCellStyle(s, "D"+row, "D"+row, redStyle)
+			f.SetCellStyle(s, "E"+row, "E"+row, redStyle)
 			continue
 		}
 
 		// If the value is an error, then note the error
 		if err, ok := raw.(error); ok {
-			f.SetCellValue(s, "C"+row, fmt.Sprintf("ERROR: %s", err))
-			f.SetCellValue(s, "D"+row, "no")
+			f.SetCellValue(s, "D"+row, fmt.Sprintf("ERROR: %s", err))
+			f.SetCellValue(s, "E"+row, "no")
 			f.SetCellStyle(s, "A"+row, "A"+row, redStyle)
 			f.SetCellStyle(s, "B"+row, "B"+row, redStyle)
 			f.SetCellStyle(s, "C"+row, "C"+row, redStyle)
 			f.SetCellStyle(s, "D"+row, "D"+row, redStyle)
+			f.SetCellStyle(s, "E"+row, "E"+row, redStyle)
 			continue
 		}
 
 		// If the value is a license, then mark the license
 		if lic, ok := raw.(*license.License); ok {
-			f.SetCellValue(s, fmt.Sprintf("C%d", i+2), lic.String())
+			if lic != nil {
+				f.SetCellValue(s, fmt.Sprintf("C%d", i+2), lic.SPDX)
+			}
+			f.SetCellValue(s, fmt.Sprintf("D%d", i+2), lic.String())
 			if o.Config != nil {
 				switch o.Config.Allowed(lic) {
 				case config.StateAllowed:
-					f.SetCellValue(s, fmt.Sprintf("D%d", i+2), "yes")
+					f.SetCellValue(s, fmt.Sprintf("E%d", i+2), "yes")
 					f.SetCellStyle(s, "A"+row, "A"+row, greenStyle)
 					f.SetCellStyle(s, "B"+row, "B"+row, greenStyle)
 					f.SetCellStyle(s, "C"+row, "C"+row, greenStyle)
 					f.SetCellStyle(s, "D"+row, "D"+row, greenStyle)
+					f.SetCellStyle(s, "E"+row, "E"+row, greenStyle)
 
 				case config.StateDenied:
-					f.SetCellValue(s, fmt.Sprintf("D%d", i+2), "no")
+					f.SetCellValue(s, fmt.Sprintf("E%d", i+2), "no")
 					f.SetCellStyle(s, "A"+row, "A"+row, redStyle)
 					f.SetCellStyle(s, "B"+row, "B"+row, redStyle)
 					f.SetCellStyle(s, "C"+row, "C"+row, redStyle)
 					f.SetCellStyle(s, "D"+row, "D"+row, redStyle)
+					f.SetCellStyle(s, "E"+row, "E"+row, redStyle)
 				}
 			}
 		}
